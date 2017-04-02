8c8a33b66e231bab5ce58fe145235467ed142e15
Archit-1 <architsamnol@gmail.com>
2017-04-02 22:03:14 +0530

    New Edited Acknowledgement (After completion of whole Donation Process)

--- app/src/main/java/ngo/donate/project/app/donatengo/Acknowledgement.java
+++ app/src/main/java/ngo/donate/project/app/donatengo/Acknowledgement.java
@@ -1,37 +1,29 @@
 package ngo.donate.project.app.donatengo;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.creativityapps.gmailbackgroundlibrary.BackgroundMail;
 
 public class Acknowledgement extends AppCompatActivity {
 
-    private String our_username = "jeevanmru@gmail.com";
-    private String our_password = "bahubali";
-    String name_donator;
-    String ngo_name;
-    String user_email;
-    String mail_subject;
-    String mail_body;
-    String donated_item_name;
-    String donated_item_quantity;
+    private final String our_username = "jeevanmru@gmail.com";
+    private final String our_password = "bahubali";
+    private  String mail_subject;
+    private  String mail_body;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    public void sendConfirmationMail(String name,String ngo_name, String user_email,String donated_item_name,String donated_item_quantity) {
 
         mail_subject = "Donation Successfull - Your Donation with " + ngo_name + " has " +
                 "been successfull.";
 
-        mail_body = "Hi " + name_donator + ",\n\nWe are pleased to inform you that your items " +
+        mail_body = "Hi " + name + ",\n\nWe are pleased to inform you that your items " +
                 "have successfully been donated to the needy people via " + ngo_name + ". This " +
                 "successfully completes your donation. Thank you for choosing our service.\n\n"
                 + "The summary of items donated is mentioned below.\n\n" + donated_item_name +
                 "\n" + donated_item_quantity;
 
-        BackgroundMail.newBuilder(Acknowledgement.this)
+        BackgroundMail.newBuilder(this)
                 .withUsername(our_username)
                 .withPassword(our_password)
                 .withMailto(user_email)
@@ -42,12 +34,14 @@ public class Acknowledgement extends AppCompatActivity {
                     @Override
                     public void onSuccess() {
                         //do some magic
+                       // Toast.makeText(Acknowledgement.this, "You will soon get an email about the confirmation", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .withOnFailCallback(new BackgroundMail.OnFailCallback() {
                     @Override
                     public void onFail() {
                         //do some magic
+                        //Toast.makeText(Acknowledgement.this, "Failed... Try Again", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .send();
